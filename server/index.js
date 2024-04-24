78ea7c395c9bd06c995df474656e9f48f6a0d864
asaMitaka <cepelits@gmail.com>
2024-04-25 02:48:54 +0300

    + app

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 
-const app = express();
-const PORT = 3000;
+const schema = require('./schema');
+const { note, notes } = require('./resolvers/query');
+const { newNote, updateNote, deleteNote } = require('./resolvers/mutation');
 
+const app = express();
 app.use(cors());
 
+const root = {
+  note,
+  notes,
+  newNote,
+  updateNote,
+  deleteNote,
+};
+
 app.use(
-  '/api',
+  '/graphql',
   graphqlHTTP({
     graphiql: true,
+    rootValue: root,
     schema: schema,
   }),
 );
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+module.exports = app;
